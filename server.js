5c96b3be51098f715febc07f0d1c7d17984de4c8
Glitch (freecodecamp-timestamp-sondregj) <none>
2019-04-30 23:09:43 +0000

    😹🚥 Updated with Glitch

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -18,7 +18,6 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
@@ -26,20 +25,16 @@ app.get("/api/hello", function (req, res) {
 
 app.get('/api/timestamp/:date_string?', (req, res, next) => {
   const dateString = req.params['date_string']
-  let parsedDate
   
-  if (!dateString) {
-    parsedDate = new Date() 
-  } else {
-    parsedDate = new Date(dateString)
-  }
+  let parsedDate = dateString
+    ? new Date(dateString)
+    : new Date()
   
   return res.send({
     unix: parsedDate.getTime(),
     utc: parsedDate.toUTCString()
   })
 })
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
